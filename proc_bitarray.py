a23163d2c9fccd173c0fa84fa2f732894831cbee
Sy <rado.skoviera@gmail.com>
2020-01-09 17:33:44 +0100

    Fully functional bitarray implementation

--- proc_bitarray.py
+++ proc_bitarray.py
@@ -2,11 +2,14 @@ import numpy as np
 import h5py
 from timeit import repeat
 from functools import partial
+import sys
 import bitarray
 
 
-filename_pattern = "bits_1000x65536_{}_mutate.hdf5"
-# filename_pattern = "bits_10000x262144_{}_mutate.hdf5"
+# filename_pattern = "data/bits_1000x65536_{}_mutate.hdf5"
+filename_pattern = "data/bits_10000x262144_{}_mutate.hdf5"
+
+storage_types = ["bool", "byte", "uint32"]
 
 
 def compute_xor(vector, matrix):
@@ -14,20 +17,39 @@ def compute_xor(vector, matrix):
     return result
 
 
-def compute_nonzero(bits):
-    return np.count_nonzero(bits, 1)
+def compute_bits(xor, storage, vector_length):
+    # unpack or pack the data in the xor matrix so that they can be further processed (bits counted)
+    if storage == "bool":
+        result = np.packbits(xor, axis=1)
+    elif storage == "byte":
+        result = xor
+    elif storage == "uint32":
+        order = sys.byteorder  # each 4 bytes have to be reversed if byteorder is "little"
+        if order == "little":
+            result = xor.view(np.uint8).reshape(-1, 4)[:, ::-1].reshape(-1, vector_length)
+        elif order == "big":
+            result = xor.view(np.uint8)
+
+    return result
+
+
+def bitifize(vec):
+    b = bitarray.bitarray(endian="big")
+    b.frombytes(vec.tobytes())
+    return b
+
+
+def compute_nonzero(bits):  # i.e. bit count / pop count
+    return [bitifize(v).count() for v in bits]
 
 
 def hamming(vector, matrix, vector_length):
     # compute XOR
-    xor = np.bitwise_xor(vector.T, matrix)
+    xor = compute_xor(vector, matrix)
 
-    if storage == "bool":
-        result = np.sum(xor, 1)
-    elif storage == "byte":
-        result = np.count_nonzero(np.unpackbits(xor[:, ::-1]).reshape(-1, vector_length).astype(np.bool), 1)
-    elif storage == "uint32":
-        result = np.count_nonzero(np.unpackbits(xor.view(np.uint8)[:, ::-1]).reshape(-1, vector_length).astype(np.bool), 1)
+    bits = compute_bits(xor, storage, vector_length)
+
+    result = compute_nonzero(bits)
 
     return result
 
@@ -39,37 +61,43 @@ def run_stats(run_list, runs):
     return f"avg = {mean} (std = {std})"
 
 
-storage = "byte"
-filename = filename_pattern.format(storage)
-print(f"Computing runtimes for {storage} storage.")
+for storage in storage_types:
+    filename = filename_pattern.format(storage)
+    print(f"Computing runtimes for {storage} storage.")
 
-with h5py.File(filename, "r") as f:
-    vector = f["vector"][()]
-    matrix = f["matrix"][()]
-    info = dict()
-    info.update(**f.attrs)
+    try:
+        with h5py.File(filename, "r") as f:
+            vector = f["vector"][()]
+            matrix = f["matrix"][()]
+            info = dict()
+            info.update(**f.attrs)
+    except IOError:
+        print("File with data not found, skipping.")
+        continue
 
-vector = np.unpackbits(xor[:, ::-1]).reshape(-1, vector_length).astype(np.bool)
-# result = hamming(vector, matrix)
+    bit_vector = bitarray.bitarray(endian="big")
+    bit_vector.frombytes(vector.tobytes())
+    bit_matrix = bitarray.bitarray(endian="big")
+    bit_matrix.frombytes(vector.tobytes())
+    # result = hamming(vector, matrix)
 
+    result = hamming(vector, matrix, info["vector_length"])
+    print(f"Result check: {np.unique(result)}")
 
-result = hamming(vector, matrix, info["vector_length"])
-print(f"Result check: {np.unique(result)}")
+    setup = "from __main__ import hamming"
+    n_runs = 100
+    n_reps = 3
 
-setup = "from __main__ import hamming"
-n_runs = 100
-n_reps = 3
+    time_result = repeat(partial(compute_xor, vector, matrix), setup=setup, repeat=n_reps, number=n_runs)
+    print(f"XOR computation runtime: {run_stats(time_result, n_runs)}")
+    xor = compute_xor(vector, matrix)
 
-time_result = repeat(partial(compute_xor, vector, matrix), setup=setup, repeat=n_reps, number=n_runs)
-print(f"XOR computation runtime: {run_stats(time_result, n_runs)}")
+    time_result = repeat(partial(compute_bits, xor, storage, info["vector_length"]), setup=setup, repeat=n_reps, number=n_runs)
+    print(f"Bit unpacking runtime: {run_stats(time_result, n_runs)}")
+    bits = compute_bits(xor, storage, info["vector_length"])
 
-xor = compute_xor(vector, matrix)
-time_result = repeat(partial(compute_bits, xor, storage, info["vector_length"]), setup=setup, repeat=n_reps, number=n_runs)
-print(f"Bit unpacking runtime: {run_stats(time_result, n_runs)}")
+    time_result = repeat(partial(compute_nonzero, bits), setup=setup, repeat=n_reps, number=n_runs)
+    print(f"Active bit counting runtime: {run_stats(time_result, n_runs)}")
 
-bits = compute_bits(xor, storage, info["vector_length"])
-time_result = repeat(partial(compute_nonzero, bits), setup=setup, repeat=n_reps, number=n_runs)
-print(f"Active bit counting runtime: {run_stats(time_result, n_runs)}")
-
-time_result = repeat(partial(hamming, vector, matrix, info["vector_length"]), setup=setup, repeat=n_reps, number=n_runs)
-print(f"Hamming distance total runtime: {run_stats(time_result, n_runs)}")
+    time_result = repeat(partial(hamming, vector, matrix, info["vector_length"]), setup=setup, repeat=n_reps, number=n_runs)
+    print(f"Hamming distance total runtime: {run_stats(time_result, n_runs)}")
