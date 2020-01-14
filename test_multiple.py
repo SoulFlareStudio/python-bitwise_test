9f7ff8be17f3b0c30e8e347f4e47e4ae7dbe0934
Sy <rado.skoviera@gmail.com>
2020-01-14 12:17:57 +0100

    Added skipping option

--- test_multiple.py
+++ test_multiple.py
@@ -11,10 +11,12 @@ import argparse
 implementations = {
     "numpy": {  # name of test/method to be shown
         # name of the script including the testing function (without ".py")
-        "script": "proc_numpy"
+        "script": "proc_numpy",
+        "skip": False
     },
     "bitarray": {
-        "script": "proc_bitarray"
+        "script": "proc_bitarray",
+        "skip": False
     }
 }
 
@@ -25,13 +27,14 @@ n_reps = 3
 
 stamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-section_names = ["xor", "bit conversion", "bit count", "hamming distance"]
+section_names = ["xor", "bit conversion", "bit count", "quantization", "hamming distance"]
 column_names = [a + b for a, b in product(section_names, ["", " STD"])]
 storage_types = ["bool", "byte", "uint32"]
 
 n_vector = 1000
 n_elements = 2**16
 datapath = "data"
+randomization = "mutate"
 
 # Parsing console inputs
 parser = argparse.ArgumentParser()
@@ -42,7 +45,9 @@ parser.add_argument("--path", "-p", default=datapath,
 parser.add_argument("--n_vector", "-nv", default=n_vector,
                     help="Number of vectors in data. Used to locate the correct datafile. Python expressions can be used, i.e. 1e3 == 1000.")
 parser.add_argument("--n_elements", "-ne", default=n_elements,
-                    help="Number of elements in each vector (i.e. vector length). Used to locate the correct datafile. . Python expressions can be used, i.e. 2**16 == 65536")
+                    help="Number of elements in each vector (i.e. vector length). Used to locate the correct datafile. Python expressions can be used, i.e. 2**16 == 65536")
+parser.add_argument("--randomization", "-rnd", choices=["mutate", "full", "sparse"], default=randomization,
+                    help="Specify which randomization method was used to generate the data. Used to locate the correct datafile. Default is 'mutate'.")
 
 args = parser.parse_args()
 
@@ -52,7 +57,7 @@ n_vector = int(eval(args.n_vector))
 n_elements = int(eval(args.n_elements))
 
 # Input/output data handling
-filename_pattern = os.path.join(datapath, f"bits_{n_vector}x{n_elements}_{{}}_mutate.hdf5")
+filename_pattern = os.path.join(datapath, f"bits_{n_vector}x{n_elements}_{{}}_{randomization}.hdf5")
 df = pd.DataFrame(columns=column_names, index=pd.MultiIndex.from_product((implementations.keys(), storage_types)))
 
 # Pre-loop setups
@@ -61,6 +66,10 @@ current_storage = storage_types[0]
 
 # Main loop
 for (implementation, impl_info) in implementations.items():
+    if impl_info["skip"]:
+        print(f"Skipping '{implementation}'.")
+        continue
+
     print(f"--------------------\nRunning test for '{implementation}'...")
     test_script = import_module(impl_info["script"])  # not very nice but works
     for output in test_script.run_tests(filename_pattern, storage_types, n_runs, n_reps):
